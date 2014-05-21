8126c49d842f723a5ff5c6f83df7e7a9ca299360
Morgas <morgas01@web.de>
2014-05-21 20:38:53 +0200

    rename

--- src/Morgas.Board.GUIElement.Menu.js
+++ src/Morgas.Board.GUIElement.Menu.js
@@ -12,16 +12,26 @@
 			this.superInit(GUI,layer);
 			this.domElement.classList.add("Menu");
 			
-			this.createListener("activeChanged selected")
+			this.createListener("activeChanged selectionChanged")
+
+			this.type=param.type||MENU.Types.VERTICAL;
+			this.axes=param.axes||[];
+			this.buttons=param.buttons||[];
+			this.domItems=[];
+			
+			this.lastdirection4=0;
+			this.lastButton=null;
 			
 			param=param||{};
 			this.menu=new SC.MENU(param);
-
-			this.type=param.type||;
-			this.axes=param.axes||[];
-			this.buttons=param.buttons||[];
 			
-			this.lastdirection4=0;
+			if(param.items)
+			{
+				for(var i=0;i<param.items.length;i++)
+				{
+					this.addItem(param.items[i]);
+				}
+			}
 		},
 		onAxis:function(type,player,index,axis)
 		{
@@ -47,8 +57,43 @@
 		},
 		onButton:function(type,player,index,value)
 		{
-			
+			if(this.buttons.length==0||this.buttons.indexOf(index)!==-1)
+			{
+				if(this.lastButton===index&&value===0)
+				{
+					this.lastButton=null;
+				}
+				else if (this.lastButton===null)
+				{
+					this.menu.toggleActive();
+					this.lastButton=index;
+				}
+			}
 		},
+		addItem:function(item)
+		{
+			var domItem=document.createElement("span");
+			domItem.innerHTML=item;
+			this.domItems.push(domIdem);
+			this.menu.addItem(item);
+			
+			this.domELement.appendChild(domItem);
+		},
+		removeItem:function(item)
+		{
+			var index=this.menu.removeItem(item);
+			if(index!==-1)
+			{
+				this.domItems.splice(index,1);
+			}
+			return index;
+		},
+		getItem:function(index)
+		{
+			var rtn=this.menu.getItem(index);
+			rtn.domElement=this.domItems[index];
+			return rtn;
+		}
 		
 	});
 	MENU.Types={
