c451f6da1839aad7223dad1646698c03a6bc15f3
Morgas01 <morgas01@web.de>
2014-11-05 15:54:30 +0100

    typo

--- src/TalePlay.Dependencies.js
+++ src/TalePlay.Dependencies.js
@@ -46,31 +46,31 @@
             uses:[]
         },
     
-        "GUI/Morgas.Board:GUIElement.js":{
+        "GUI/Morgas.Board.GUIElement.js":{
             deps:[MORGAS_PATH+"Morgas.js","Morgas.Board.js"],
             uses:[MORGAS_PATH+"Morgas.NodePatch.js"]
         },
-        "GUI/Morgas.Board:GUIElement.Menu.js":{
-            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board:GUIElement.js"],
+        "GUI/Morgas.Board.GUIElement.Menu.js":{
+            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board.GUIElement.js"],
             uses:["Morgas.Menu.js","css/structure/GUIElement.Menu.css"]
         },
         "GUI/Morgas.Board.GUIElement.ControllerConfig.js":{
-            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board:GUIElement.js"],
+            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board.GUIElement.js"],
             uses:["Morgas.Controller.js","Morgas.Controller.Mapping.js","css/structure/GUIElement.ControllerConfig.css"]
         },
         "GUI/Morgas.Board.GUIElement.ControllerManager.js":{
-            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board:GUIElement.js"],
+            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board.GUIElement.js"],
             uses:["Morgas.Menu.js","Morgas.Controller.js","Morgas.Controller.Keyboard.js","Morgas.Controller.Gamepad.js","Morgas.Controller.Mapping.js",
-                "GUI/Morgas.Board:GUIElement.Menu.js","GUI/Morgas.Board.GUIElement.ControllerConfig.js","css/structure/GUIElement.ControllerManager.css"]
+                "GUI/Morgas.Board.GUIElement.Menu.js","GUI/Morgas.Board.GUIElement.ControllerConfig.js","css/structure/GUIElement.ControllerManager.css"]
         },
-        "GUI/Morgas.Board:GUIElement.Map.js":{
-            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board:GUIElement.js","Morgas.Map.js"],
+        "GUI/Morgas.Board.GUIElement.Map.js":{
+            deps:[MORGAS_PATH+"Morgas.js","GUI/Morgas.Board.GUIElement.js","Morgas.Map.js"],
             uses:[MORGAS_PATH+"Morgas.util.object.js","Math/Morgas.Math.Point.js"]
         },
 
         "Morgas.Board.MapMaker.js":{
             deps:[MORGAS_PATH+"Morgas.js","Morgas.Board.Layer.js"],
-            uses:["Morgas.Map.js","GUI/Morgas.Board:GUIElement.Map.js","Morgas.Menu.js","GUI/Morgas.Board:GUIElement.Menu.js",MORGAS_PATH+"Morgas.util.object.js"]
+            uses:["Morgas.Map.js","GUI/Morgas.Board.GUIElement.Map.js","Morgas.Menu.js","GUI/Morgas.Board.GUIElement.Menu.js",MORGAS_PATH+"Morgas.util.object.js"]
         }
     });
 
