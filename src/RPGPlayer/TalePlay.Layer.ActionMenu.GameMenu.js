6c48c996b95cf48cda225f68fd52a83cc9eaf5b9
Morgas <morgas01@web.de>
2015-01-13 23:07:25 +0100

    StartMenu & GameMenu

---
+++ src/RPGPlayer/TalePlay.Layer.ActionMenu.GameMenu.js
@@ -0,0 +1,75 @@
+(function(µ,SMOD,GMOD){
+	
+	//TODO change to Layer
+	
+	var AMENU=GMOD("Layer.ActionMenu");
+	
+	
+	var GMENU=AMENU.GameMenu=µ.Class(AMENU,{
+		init:function(param)
+		{
+			param=param||{};
+			
+			this.superInit(AMENU,{
+				styleClass:["panel"],
+				actions:[
+					{
+						text:"save",
+						action:"saveGame",
+						data:param.saveData
+					},
+					{
+						text:"close",
+						action:"close"
+					}
+				],
+			});
+
+			this.domElement.classList.add("GameMenu");
+			this.createListener("start close");
+		
+			this.persistanceLayer=(typeof param.persistanceLayer==="function")?param.persistanceLayer:GMOD(param.persistanceLayer||"Layer.Persistance");
+			this.dbConn=param.dbConn;
+			this.saveConverter=param.saveConverter;
+
+		},
+		onController:function(event)
+		{
+			if(event.type==="buttonChanged"&&event.value==1)
+			{
+				switch(event.index)
+				{
+					case 1:
+						this.menu.setActive(1);
+						break;
+					case 2:
+						AMENU.prototype.onController.call(this,event);
+						break;
+				}
+			}
+			else
+			{
+				AMENU.prototype.onController.call(this,event);
+			}
+		},
+		saveGame:function(item)
+		{
+			if(item.data)
+			{
+				var p=new this.persistanceLayer({
+					dbConn:this.dbConn,
+					saveClass:this.saveClass,
+					saveConverter:this.saveConverter,
+					saveData:item.data
+				});
+				this.board.addLayer(p);
+			}
+		},
+		close:function()
+		{
+			this.fire("close");
+		}
+	});
+	SMOD("RPGPlayer.GameMenu",GMENU);
+	
+})(Morgas,Morgas.setModule,Morgas.getModule);
---
+++ src/TalePlay.Layer.Persistance.js
@@ -0,0 +1,125 @@
+(function(µ,SMOD,GMOD){
+	
+	//TODO change to Layer
+	
+	var LAYER=GMOD("Layer");
+	
+	var SC=GMOD("shortcut")({
+		rs:"rescope",
+		Menu:"GUI.Menu",
+		debug:"debug",
+		donwload:"download"
+	});
+	
+	var PERSISTANCE=LAYER.Persistance=µ.Class(LAYER,{
+		init:function(param)
+		{
+			param=param||{};
+			
+			this.superInit(LAYER,{mode:LAYER.Modes.LAST});
+			SC.rs.all(["_fillMenu"],this);
+			
+			this.createListener("load");
+
+			this.domElement.classList.add("Persistance");
+			
+			this.dbConn=param.dbConn;
+			this.saveClass=param.saveClass;
+			this.saveData=param.data;
+			
+			this.menu=new SC.Menu({
+				styleClass:param.styleClass,
+				active:param.active||0,
+				loop:param.loop===true,
+				selectionType:SC.Menu.SelectionTypes.NONE,
+				converter:param.saveConverter
+			});
+			
+			this.add(this.menu);
+			this.menu.addListener("select",this,this._onSelect);
+			
+			this._update();
+		},
+		onController:function(event)
+		{
+			if(event.type==="buttonChanged"&&event.value==1)
+			{
+				switch(event.index)
+				{
+					case 1:
+						if(this.GUIElements.length>1)this.GUIElements[1].setActive(3);//submenu
+						else this.destroy();
+						break;
+					case 2:
+						LAYER.prototype.onController.call(this,event);
+						break;
+				}
+			}
+			else
+			{
+				LAYER.prototype.onController.call(this,event);
+			}
+		},
+		_update:function()
+		{
+			this.dbConn.load(this.saveClass).then(this._fillMenu);
+			return null;
+		},
+		_fillMenu:function(results)
+		{
+			this.menu.clear();
+			results.sort(function(a,b){return a.getID()-b.getID();});
+			results.push(null);
+			this.menu.addAll(results);
+			if(this.menu.getActive().index===-1)this.menu.setActive(0);
+			return null;
+		},
+		_onSelect:function(event)
+		{
+			if(event.value)
+			{
+				var subMenu=new SC.Menu({
+					items:[
+					   this.saveData ? "Save" : "Load",
+					   "Export",
+					   "Delete",
+					   "Cancel"
+					],
+					active:0,
+					loop:false,
+					selectionType:SC.Menu.SelectionTypes.NONE
+				});
+				subMenu.addListener("select",this,this._onSubSelect);
+				this.add(subMenu);
+			}
+			else if (this.saveData)
+			{
+				this.saveData.setID(event.index);
+				this.dbConn.save([this.saveData]).then(this._update());
+			}
+		},
+		_onSubSelect:function(event)
+		{
+			switch (event.value)
+			{
+				case "Load":
+					this.fire("load",{save:this.menu.getActive().value.getData()})
+					break;
+				case "Save":
+					this.dbConn.save([this.saveData]).then(this._update());
+					break;
+				case "Export":
+					SC.download(JSON.stringify(this.menu.getActive().value));
+					break;
+				case "Delete":
+					this.dbConn["delete"]([this.saveData]).then(this._update());
+					break;
+				case "Cancel":
+					break;
+			}
+			event.source.destroy();
+		}
+	});
+	SMOD("Layer.Persistance",PERSISTANCE);
+	
+})(Morgas,Morgas.setModule,Morgas.getModule);
